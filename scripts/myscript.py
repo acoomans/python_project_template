03d584e4ec138d572f278af16b60fcdf2fc6039a
Arnaud Coomans <arnaud.coomans@gmail.com>
2018-12-01 15:02:33 -0500

    Relative import path for when developing

--- scripts/myscript.py
+++ scripts/myscript.py
@@ -1,7 +1,17 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
+
+import os
+import sys
+
+
+parent_dir = os.path.split(os.path.dirname(__file__))[-1]
+if dir == 'scripts':
+    sys.path.append(os.path.join(parent_dir, '..'))
+
 
 def main():
     print("hello")
+
 
 if __name__ == "__main__":
     main()
